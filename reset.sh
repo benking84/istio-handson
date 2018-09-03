61dfcb182a3eb02ac2d081d84f26368a37699e5e
[benjking] <[benjking@google.com]>
2018-09-03 15:33:30 +1000

    initial commit

---
+++ reset.sh
@@ -0,0 +1,16 @@
+#This script will reset the bookingo demo to default settings with all traffic going to v1 (no stars) Questions: benjking@
+#Delete all destionation rules
+./istioctl delete destinationrule details
+./istioctl delete destinationrule ratings
+./istioctl delete destinationrule reviews
+./istioctl delete destinationrule productpage
+
+#Delete all virtual service config
+./istioctl delete virtualservice reviews
+./istioctl delete virtualservice ratings
+./istioctl delete virtualservice details
+./istioctl delete virtualservice productpage
+
+#apply default destination rules and virtual services
+kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml
+kubectl apply -f samples/bookinfo/networking/virtual-service-all-v1.yaml
